500064d5fa2f4d65f1295f2af7714ed19e7f4f59
satoyu <yuuutaas124@gmail.com>
2022-12-17 17:08:29 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/jec/cm0119/mamoru/MainActivity.kt
@@ -0,0 +1,11 @@
+package jp.ac.jec.cm0119.mamoru
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
